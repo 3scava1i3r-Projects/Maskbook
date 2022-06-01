debe5a0e7432d0be73c281d70cd7dc7e44abf17a
Lantt <laanfor@gmail.com>
2022-06-01 16:14:35 +0800

    feat: add element anchor

--- packages/plugins/EVM/src/state/Hub/hub.ts
+++ packages/plugins/EVM/src/state/Hub/hub.ts
@@ -175,7 +175,7 @@ class Hub implements EVM_Hub {
                 size: this.sizePerPage,
             })
 
-            yield yield* pageable.data
+            yield* pageable.data
 
             if (pageable.data.length === 0) return
         }
